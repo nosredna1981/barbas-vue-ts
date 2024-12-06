912ea0fda1630a92323d6d9dce26bde5eb82966e
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-12-05 21:55:08 -0300

    vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,92 @@
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container">
+      <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+      <img
+        class="navbar-toggler"
+        src="imagem/barbershop-logo.jpg"
+        height="40"
+        alt=""
+      />
+
+      <button
+        class="navbar-toggler bg-yellow"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">INÍCIO</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html#services">SERVIÇOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html#about">SOBRE NÓS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html#contact">CONTATO</a>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="login.html">ENTRAR</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.navbar {
+  width: 100%;
+  backdrop-filter: blur(10px);
+  background-color: var(--shadow);
+  /* display: flex;
+    align-items: flex-start;
+    padding: 20px;
+    flex-direction: row;
+    justify-content: space-around; */
+  /* background-color: var(--black); */
+  /* position: sticky;
+    top: 0 */
+}
+/* 
+.nav-link {
+    list-style: none;
+    display: flex;
+    justify-content: flex-start;
+} */
+
+/*.nav-links li {
+        margin-right: 20px; 
+    }*/
+
+.navbar .nav-link {
+  text-decoration: none;
+  color: var(--white);
+  font-size: 1.5rem;
+  padding: 5px 15px;
+  transition: 0.3s ease;
+  border-bottom: solid 4px transparent;
+}
+.navbar .nav-link:hover,
+.navbar .nav-link:active {
+  /* background-color: var(--yellow);
+        border-radius: 5px;*/
+  color: var(--yellow);
+  border-bottom: solid 4px var(--yellow);
+  transition: 0.3s ease;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,233 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <div id="navbar-component"></div>
+
+    <header>
+      <div class="navbox">
+        <div class="header-content container">
+          <div class="header-title">
+            <h1>Barba's</h1>
+            <p>
+              Uma barbearia é um negócio que oferece serviços de corte de
+              cabelo, barba e bigode, entre outros, e que se destina a clientes
+              do sexo masculino. A história da barbearia remonta à antiguidade,
+              com registros de salões na Babilônia há mais de 5.000 anos
+            </p>
+          </div>
+
+          <div class="logo">
+            <img src="imagem/barbershop-logo.jpg" alt="logo" />
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section id="services">
+      <div class="card-container container" id="card-component"></div>
+      <!-- <div class="card">
+            <a href="#">
+                <div class="card-img">
+                    <img src="./imagem/barber-comum.png" alt="">
+
+                </div>
+                <div class="card-content">
+                    <h2>Barbas</h2>
+                    <p>
+                        é uma forma de expressar a personalidade, realcar traços do rosto e acompanhar as últimas
+                        tendências
+                        da moda masculina.
+                    </p>
+                </div>
+            </a>
+        </div>
+        <div class="card">
+            <a href="#">
+                <div class="card-img">
+                    <img src="./imagem/barber-5497152_1280.jpg" alt="">
+
+                </div>
+                <div class="card-content">
+                    <h2>Corte na Régua</h2>
+                    <p>
+                        é uma forma de expressar a personalidade, realcar traços do rosto e acompanhar as últimas
+                        tendências
+                        da moda masculina.
+                    </p>
+                </div>
+            </a>
+        </div>
+        <div class="card">
+            <a href="#">
+                <div class="card-img">
+                    <img src="./imagem/barber-cabelo.png" alt="">
+
+                </div>
+                <div class="card-content">
+                    <h2>Barba & Cabelo</h2>
+                    <p>
+                        é uma forma de expressar a personalidade, realcar traços do rosto e acompanhar as últimas
+                        tendências
+                        da moda masculina.
+                    </p>
+                </div>
+            </a>
+        </div>
+        <div class="card">
+            <a href="#">
+                <div class="card-img">
+                    <img src="./imagem/barber-vikings1.png" alt="">
+
+                </div>
+                <div class="card-content">
+                    <h2>Estilo Vikings</h2>
+                    <p>
+                        é uma forma de expressar a personalidade, realcar traços do rosto e acompanhar as últimas
+                        tendências
+                        da moda masculina.
+                    </p>
+                </div>
+            </a>
+        </div>
+        <div class="card">
+            <a href="#">
+                <div class="card-img">
+                    <img src="./imagem/barber-barbacabeloebigode.png" alt="">
+
+                </div>
+                <div class="card-content">
+                    <h2>Barba, Cabelo & Bigode</h2>
+                    <p>
+                        é uma forma de expressar a personalidade, realcar traços do rosto e acompanhar as últimas
+                        tendências
+                        da moda masculina.
+                    </p>
+                </div>
+            </a>
+        </div>
+        <div class="card">
+            <a href="#">
+                <div class="card-img">
+                    <img src="./imagem/barber-moderno.png" alt="">
+
+                </div>
+                <div class="card-content">
+                    <h2>Modernno</h2>
+                    <p>
+                        é uma forma de expressar a personalidade, realcar traços do rosto e acompanhar as últimas
+                        tendências
+                        da moda masculina.
+                    </p>
+                </div>
+            </a>
+        </div> -->
+    </section>
+
+    <section class="bg-black container" id="about">
+      <section class="menu-container">
+        <div class="menu-itens">
+          <img src="imagem/barbershop-logo.jpg" alt="img" />
+          <p>comentario.</p>
+        </div>
+        <div class="menu-itens">
+          <img src="imagem/barbershop-logo.jpg" alt="img" />
+          <p>comentario.</p>
+        </div>
+        <div class="menu-itens">
+          <img src="imagem/barbershop-logo.jpg" alt="img" />
+          <p>comentario</p>
+        </div>
+      </section>
+    </section>
+    <section class="bg container" id="contact">
+      <!--yellow container expande-->
+      <div class="contact">
+        <div class="contact-map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3675.356514534342!2d-43.56141262383389!3d-22.900217479259787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9be15839e68c4f%3A0x588a284ae162bc38!2sSenac%20Campo%20Grande!5e0!3m2!1spt-BR!2sbr!4v1727395368333!5m2!1spt-BR!2sbr"
+            width="98%"
+            height="300"
+            style="border: 0"
+            allowfullscreen="false"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+          <div>
+            <p class="bolder white">Rua barbas Cortadas, 789 Campo Grande</p>
+            <P>
+              <span class="bolder number phone white">(21)5555-5555</span>
+            </P>
+          </div>
+        </div>
+
+        <div class="contact-form">
+          <form>
+            <div class="form-group">
+              <!-- <label for="nome">Nome</label> -->
+              <input
+                type="text"
+                name="nome"
+                id="nome"
+                placeholder="Nome"
+                required
+              />
+            </div>
+
+            <div class="form-group">
+              <!-- <label for="email">E-mail</label> -->
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="E-mail"
+                required
+              />
+            </div>
+
+            <div class="form-group">
+              <!-- <label for="tel">Telefone</label> -->
+              <input
+                type="tel"
+                name="tel"
+                id="tel"
+                placeholder="Telefone"
+                required
+              />
+            </div>
+
+            <div class="form-group">
+              <select name="assunto" aria-placeholder="assunto">
+                <option disabled selected>Assunto</option>
+                <option value="contato">Contato</option>
+                <option value="reclame">Reclamação</option>
+                <option value="sugent">Sugestão</option>
+              </select>
+            </div>
+
+            <div class="form-group">
+              <textarea
+                name="mensagem"
+                id="mensagem"
+                rows="5"
+                placeholder="Mensagem"
+                required
+              ></textarea>
+            </div>
+
+            <div class="form-group right">
+              <!-- <input type="submit" value="Enviar"> -->
+              <button>Enviar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+
+    <footer></footer>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<script lang="ts"></script>
 
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+<style scoped>
+@import "@/assets/css/home.css";
+</style>
